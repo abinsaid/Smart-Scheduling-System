465f81cb2af18f4440638b1ec7c8406d45a6f366
Abdullah Binsaid <abinsaid77@gmail.com>
2022-01-26 07:02:03 +0300

    Update App.js
    doing some stuff for making sure

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textStyles}>Hello Dr Ryan!</Text>
+      <Text style={styles.textStyles}>Welcome to CPIT-499</Text>
+      <Text style={styles.textStyles}>This is after using github</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#a2d2ff',
     alignItems: 'center',
     justifyContent: 'center',
+  
+  },
+  textStyles:{
+   fontSize: 50,
+   fontFamily:'Ariel'
+   
+
   },
 });
